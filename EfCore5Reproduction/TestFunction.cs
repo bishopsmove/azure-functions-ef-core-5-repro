d5d9cfa91fe9c71027ced1d97ac15f28794ba6db
Jim Ownby <bishop@ownby.net>
2020-12-31 11:29:37 -0500

    Adjusted for EFCore 3.1.10

--- EfCore5Reproduction/TestFunction.cs
+++ EfCore5Reproduction/TestFunction.cs
@@ -1,14 +1,39 @@
 ﻿using Microsoft.Azure.WebJobs;
-using System.Net.Http;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 
 namespace EfCore5Reproduction
 {
     public class TestFunction
     {
         [FunctionName("TestFunction")]
-        public void Exec([HttpTrigger] HttpRequestMessage req)
-        {
+        // public void Exec([HttpTrigger] HttpRequestMessage req)
+        // {
 
+        // }
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
         }
     }
 }
